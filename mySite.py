2f0755da978582481fefd72ecab6845f9b1ee04a
Navarro44 <jorgenavarrogracia@gmail.com>
2024-05-07 19:56:44 -0600

    Updated descriptions

--- mySite.py
+++ mySite.py
@@ -14,12 +14,13 @@ def menu(name):
         st.header("_Process and Research_")
         filing_image = os.path.join("images/10k.png")
         st.image(filing_image, width=600)
-        st.write("Brazil is the most successful team in World Cup history, having won the tournament a record five times (1958, 1962, 1970, 1994, and 2002). Their style of play has often been characterized by flair, creativity, and technical skill. Brazil is known for its 'jogo bonito' (beautiful game) approach, emphasizing skillful dribbling, quick passing, and attacking football.")
+        st.write("A 10-K filing is an annual report that publicly traded companies are required to submit to the U.S. Securities and Exchange Commission (SEC). The 10-K provides a comprehensive overview of the company's business, financial performance, and operations for the previous fiscal year. It serves as a crucial source of information for investors, analysts, and other stakeholders, offering valuable insights into the company's financial health, competitive position, and future prospects. The 10-K filing provides value to investors and other stakeholders by offering transparency into the company's operations, financial performance, and risk factors. It serves as a comprehensive source of information for making informed investment decisions, assessing the company's competitive position, and evaluating its long-term prospects.")
+
         st.divider()
         st.header("_Analytical Decisions_")
         analysis_image = os.path.join("images/analysis.png")
         st.image(analysis_image, width=600)
-        st.write("Brazil is the most successful team in World Cup history, having won the tournament a record five times (1958, 1962, 1970, 1994, and 2002). Their style of play has often been characterized by flair, creativity, and technical skill. Brazil is known for its 'jogo bonito' (beautiful game) approach, emphasizing skillful dribbling, quick passing, and attacking football.")
+        st.write("In this project, I decided to specifically take a look at the net income and basic earnings per share metrics. To me, these were the most important factors into taking consideration whether to invest on a company or not. Net income representes the amount of money left over after the business: invested into research, generated its products, paid its providers, rewarded its workers, financed its banks, and dished out government taxes. This amount is the money that belongs to the company and its shareholders, thus, based on this number, shareholders make the decision to reinvest or distribute dividends. Earnings per share represents the company's profitability by showing how much money a business makes for each share of its stock, in other words, how much the investor benefits from having a stock in the business. In the Start tab to the left, you will be able to see the progression of these two for a company of your choosing, and be provided with a LLM response providing insight into the matter.")
 
     elif name == "Start":
         st.header("Get Started")

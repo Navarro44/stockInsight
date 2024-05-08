48045c630109374fd5b161341b717c0a968eb97b
Navarro44 <jorgenavarrogracia@gmail.com>
2024-05-07 19:29:24 -0600

    Added requirements file

--- tester.py
+++ tester.py
@@ -3,16 +3,6 @@ from bs4 import BeautifulSoup
 
 
 
-with open("sec-edgar-filings/AAPL/10-K/0000320193-18-000145/full-submission.txt", "r") as f:
-    doc = BeautifulSoup(f, 'lxml')
-
-tag = doc.find_all("DocumentFiscalPeriodFocus")
-
-# Print the tag and its content
-print(tag)
-# context = metadata["contextref"]
-# focusYear = metadata.text
-
-#DocumentFiscalYearFocus
+print(print(BeautifulSoup.__version__))
 
     

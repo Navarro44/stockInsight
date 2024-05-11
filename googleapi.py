f3289e4f8ac54d0ae64ad2060ee8511630af7278
Navarro44 <jorgenavarrogracia@gmail.com>
2024-05-10 21:17:18 -0600

    Changed text error

--- googleapi.py
+++ googleapi.py
@@ -12,7 +12,7 @@ def to_markdown(text):
 def getText(userKey, ticker, netIncomeDict, earningsPerShareDict):
     genai.configure(api_key=userKey)
     model = genai.GenerativeModel('gemini-pro')
-    response = model.generate_content("You are a financial specialist and will analyze the following two dictionaries to determine if the user should invest in the " + ticker + " technology company. The first dictionary represents net income across time: " + str(netIncomeDict) +", and the second represents earnings per share across time: " str(earningsPerShareDict) +  ". First, write a paragraph calculating the growth percentage of both metrics across the years. Then write another paragrahph providing insight about the businesses performance, and a suggestion if the user should invest based on current technological news and trends.")
+    response = model.generate_content("You are a financial specialist and will analyze the following two dictionaries to determine if the user should invest in the " + ticker + " technology company. The first dictionary represents net income across time: " + str(netIncomeDict) + ", and the second represents earnings per share across time: " + str(earningsPerShareDict) +  ". First, write a paragraph calculating the growth percentage of both metrics across the years. Then write another paragrahph providing insight about the businesses performance, and a suggestion if the user should invest based on current technological news and trends.")
     return response.text
 
 
